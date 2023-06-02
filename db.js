cd8f3b8b58c2775ac94a88584b99e6a8f5ed0759
Sumit Nagra <121888119+sumitnagra@users.noreply.github.com>
2023-06-02 17:52:23 +0530

    resolving error

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const uri ="mongodb+srv://nagarsumit144:sumit0004@cluster0.l3yrn89.mongodb.net/";
+const uri ="mongodb+srv://nagarsumit144:sumit0004@cluster0.l3yrn89.mongodb.net/test";
 
 const connectMongo = async () => {
     mongoose.set('strictQuery', true);
