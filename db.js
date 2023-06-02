49ff018004bdd4728a362f6a8d6c95149c3d4f33
Sumit Nagra <121888119+sumitnagra@users.noreply.github.com>
2023-06-02 17:21:05 +0530

    strick query

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const uri ="mongodb+srv://nagarsumit144:sumit0004@cluster0.l3yrn89.mongodb.net/";
 
 const connectMongo = async () => {
+    mongoose.set('strictQuery', false);
  mongoose.connect(uri, () => {
         console.log("Connected Mongo Successfully")
     })
